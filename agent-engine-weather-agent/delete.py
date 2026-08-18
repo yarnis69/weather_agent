cb5375a8f87bc7428f114d5cb391389faf01d9ad
Matthew <91457712+yarnis69@users.noreply.github.com>
2026-08-18 12:41:56 +0100

    Revert "test of new pipeline for agent engine"
    This reverts commit 6627f0091034b0e34a29f54f95f025e1316114ce.

--- agent-engine-weather-agent/delete.py
+++ agent-engine-weather-agent/delete.py
@@ -9,13 +9,8 @@ client = vertexai.Client(
 
 all_agents = client.agent_engines.list()
 
-# find Weather_Agent by display name and assign to target_agent variable
-
-target_agent = next(
-    (agent for agent in all_agents if agent.display_name == "Weather_Agent"),
-    None
-)
+print(list(all_agents))
 
 # delete the Weather_Agent if it exists
 
-client.agent_engines.delete(name=target_agent.api_resource.name, force=True)
+client.agent_engines.delete(name=TBC, force=True)
